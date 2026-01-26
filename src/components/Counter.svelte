767b257f839f52a888be1e7f87e5db157fd40ba2
Dima Popov <popov.dmitriiy@gmail.com>
2026-01-26 16:53:58 +0100

    Initial commit

---
+++ src/components/Counter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count = $state(0);
+
+  function increment() {
+    count += 1;
+  }
+
+  function decrement() {
+    count -= 1;
+  }
+</script>
+
+<div>
+  <h2>Counter: {count}</h2>
+  <button onclick={increment}>Increment</button>
+  <button onclick={decrement}>Decrement</button>
+</div>
