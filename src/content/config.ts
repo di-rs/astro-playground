1c1343d9b3ea4122b389d0931d5990957317c4e8
Dima Popov <popov.dmitriiy@gmail.com>
2026-02-04 23:12:20 +0100

    updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,26 @@
 import { file, glob } from "astro/loaders";
 import { defineCollection, reference, z } from "astro:content";
 
+const products = defineCollection({
+  type: "data",
+  schema: z.object({
+    product_id: z.number(),
+    product_name: z.string(),
+    price: z.number(),
+    in_stock: z.boolean(),
+    colors: z.array(z.string()),
+    details: z.object({
+      brand: z.string(),
+      model: z.string(),
+      screen_size: z.number().optional(),
+      wireless: z.boolean().optional(),
+      features: z.array(z.string()),
+    }),
+  }),
+});
+
 export const collections = {
+  products,
   posts: defineCollection({
     loader: glob({
       pattern: "src/data/posts/**/*.md",
